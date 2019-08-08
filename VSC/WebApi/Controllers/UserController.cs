f160c3fe62ab559b3f27a772efb9284e97562ff6
zekiri <zekiriabd@yahoo.fr>
2019-08-08 09:41:11 -0700

    get - Post

---
+++ VSC/WebApi/Controllers/UserController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebApi.Models;
+
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        // GET api/values
+        [HttpGet]
+        public ActionResult<List<User>> GetUsers()
+        {
+            List<User> oUsers = new List<User>();
+            oUsers.Add(new User(){ UserId= 1 , FirstName="Zekiri1", LastName="Abdelali1"});
+            oUsers.Add(new User(){ UserId= 2 , FirstName="Zekiri2", LastName="Abdelali2"});
+            oUsers.Add(new User(){ UserId= 3 , FirstName="Zekiri3", LastName="Abdelali3"});
+            return  oUsers;
+        }
+
+        // GET api/values/5
+        
+        // POST api/values
+        [HttpPost]
+        public void PostUser([FromBody] User newUser)
+        {
+            Console.WriteLine(newUser.ToString());
+         
+        }
+
+       
+    }
+}
---
+++ VSC/WebApi/Models/User.cs
@@ -0,0 +1,8 @@
+namespace WebApi.Models
+{
+public class User {
+       public int UserId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+}
+}

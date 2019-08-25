061257c83095a908293e41b95bb0f4819fe42b74
zekiri <zekiriabd@yahoo.fr>
2019-08-25 10:00:15 -0700

    EF COre

---
+++ VS2019/EFCore/ORM/User.cs
@@ -0,0 +1,78 @@
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+
+public class User
+{
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+}
+
+ public class UserDBContext : DbContext {
+
+    public DbSet<User> Users { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder){
+        base.OnConfiguring(optionsBuilder);
+        optionsBuilder.UseSqlServer(@"Data Source=DESKTOP-MAGJ7HQ\SQLEXPRESS;Initial Catalog=USER_Managment;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=True;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+    }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<User>()
+            .ToTable("USERDB")
+            .HasKey(x=>x.Id);
+    }
+
+ }
+
+
+
+
+public static class UserManager
+{
+    public static List<User> UserList()
+    {
+        using (var db = new UserDBContext())
+        {
+            return db.Users.OrderBy(p => p.FirstName).ToList();
+        }
+    }
+
+    public static User UserById(int id)
+    {
+        using (var db = new UserDBContext())
+        {
+            return db.Users.Find(id);
+        }
+    }
+
+    public static void DelUserById(int id)
+    {
+        using (var db = new UserDBContext())
+        {
+            var user = db.Users.Find(id);
+            db.Users.Remove(user);
+            db.SaveChanges();
+        }
+    }
+
+    public static void UpdateUser(User newUser)
+    {
+        using (var db = new UserDBContext())
+        {
+            db.Users.Update(newUser);
+            db.SaveChanges();
+        }
+    }
+
+    public static void AddUser(User newUser)
+    {
+        using (var db = new UserDBContext())
+        {
+            db.Users.Add(newUser);
+            db.SaveChanges();
+        }
+    }
+}
---
+++ VS2019/EFCore/WebApi/Controllers/UserController.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+using WebApi.Models;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<List<UserM>> GetUsers()
+        {
+            var UsersDB = UserManager.UserList();
+            var Result = new List<UserM>();
+            foreach (var userDB in UsersDB)
+            {
+                Result.Add(new UserM()
+                {
+                    User_Id = userDB.Id,
+                    FirstName = userDB.FirstName,
+                    LastName = userDB.LastName
+                });
+            }
+            return Result;
+
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<UserM> GetUserByID(int id)
+        {
+            var userDB = UserManager.UserById(id);
+            var Result = new UserM() {
+                User_Id = userDB.Id,
+                FirstName = userDB.FirstName,
+                LastName = userDB.LastName
+            };
+
+            return Result;
+        }
+
+        [HttpGet("{id}")]
+        public void DelUserByID(int id)
+        {
+            UserManager.DelUserById(id);
+        }
+
+        [HttpPost]
+        public void PostUser([FromBody] UserM newUser)
+        {
+            var user = new User()
+            {
+                FirstName = newUser.FirstName,
+                LastName = newUser.LastName,
+            };
+
+           UserManager.AddUser(user);
+        }
+
+        [HttpPost]
+        public void UpdateUser([FromBody] User newUser)
+        {
+            UserManager.UpdateUser(newUser);
+        }
+
+    }
+}

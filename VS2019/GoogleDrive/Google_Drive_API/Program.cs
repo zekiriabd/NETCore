d2f1b9cd784e67a18b9ef09aed644b3269c9552f
zekiri <zekiriabd@yahoo.fr>
2020-05-20 11:35:15 +0200

    S3ApplicationConsole

--- VS2019/GoogleDrive/Google_Drive_API/Program.cs
+++ VS2019/GoogleDrive/Google_Drive_API/Program.cs
@@ -13,94 +13,128 @@ namespace Google_Drive_API
 
     class Program
     {
-        static string[] Scopes = { 
+        static string[] SCOPES = { 
             DriveService.Scope.Drive, 
             DriveService.Scope.DriveFile, 
             DriveService.Scope.DriveMetadata, 
             DriveService.Scope.DriveAppdata 
         };
 
-        static string ApplicationName = "MyGoogleConnect";
-        private static UserCredential GetUserCredential()
-        {
-            UserCredential result;
+        static  string APPLICATION_NAME = "MyGoogleConnect";
+        static string CREDENTIALS_FILE_PATH = "credentials.json";
+        static string TOKENS_DIRECTORY_PATH = "token.json";
 
-            using (var stream = new FileStream("credentials.json", FileMode.Open, FileAccess.Read))
+        private static DriveService service;
+
+        private static UserCredential GetUserCredentials()
+        {
+            UserCredential result = null;
+            try
             {
-                string credPath = "token.json";
-                result = GoogleWebAuthorizationBroker.AuthorizeAsync(
-                    GoogleClientSecrets.Load(stream).Secrets,
-                    Scopes,
-                    "user",
-                    CancellationToken.None,
-                    new FileDataStore(credPath, true)).Result;
-                Console.WriteLine("Credential file saved to: " + credPath);
+                using (var stream = new FileStream(CREDENTIALS_FILE_PATH, FileMode.Open, FileAccess.Read))
+                {
+                    ClientSecrets clientSecrets  = GoogleClientSecrets.Load(stream).Secrets;
+
+                    result = GoogleWebAuthorizationBroker.AuthorizeAsync(
+                        clientSecrets,
+                        SCOPES,
+                        "user",
+                        CancellationToken.None,
+                        new FileDataStore(TOKENS_DIRECTORY_PATH, true)).Result;
+                }
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
             }
             return result;
-
         }
 
-
-        private static void Getfiles(DriveService service)
+        private static void Getfiles()
         {
-            // Define parameters of request.
-            FilesResource.ListRequest listRequest = service.Files.List();
-            listRequest.PageSize = 10;
-            listRequest.Fields = "nextPageToken, files(id, name)";
-
-            IList<Google.Apis.Drive.v3.Data.File> files = listRequest.Execute().Files;
-            Console.WriteLine("Files:");
-            if (files != null && files.Count > 0)
+            try
             {
-                foreach (var file in files)
+                // Define parameters of request.
+                FilesResource.ListRequest listRequest = service.Files.List();
+                listRequest.PageSize = 10;
+                listRequest.Fields = "nextPageToken, files(id, name)";
+
+                IList<Google.Apis.Drive.v3.Data.File> files = listRequest.Execute().Files;
+
+                Console.WriteLine("Files:");
+                if (files != null && files.Count > 0)
                 {
-                    Console.WriteLine("{0} ({1})", file.Name, file.Id);
-                    if (file.Name == "zekiri.jpg")
-                        DownloadFile(file.Id, service);
+                    foreach (var file in files)
+                    {
+                        Console.WriteLine("{0} ({1})", file.Name, file.Id);
+                        if (file.Name == "zekiri.jpg")
+                            DownloadFile(file.Id);
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("No files found.");
                 }
             }
-            else
+            catch (Exception ex)
             {
-                Console.WriteLine("No files found.");
+                Console.WriteLine(ex.ToString());
             }
-            Console.Read();
         }
 
-        private static void DownloadFile(string fileId, DriveService service)
+        private static void DownloadFile(string fileId)
         {
-            var request = service.Files.Get(fileId);
-            var stream = new System.IO.MemoryStream();
-
-
-            request.MediaDownloader.ProgressChanged += (IDownloadProgress progress) =>
+            try
             {
-                if (progress.Status == DownloadStatus.Completed)
-                {
-                    Console.WriteLine("Download complete.");
-                    FileStream file = new FileStream("d:\\zekiri.jpg", FileMode.Create, FileAccess.Write);
-                    stream.WriteTo(file);
-                    file.Close();
-                    stream.Close();
-                }
-            };
+                var request = service.Files.Get(fileId);
+                var stream = new System.IO.MemoryStream();
 
-            request.Download(stream);
+                request.MediaDownloader.ProgressChanged += (IDownloadProgress progress) =>
+                {
+                    if (progress.Status == DownloadStatus.Completed)
+                    {
+                        FileStream file = new FileStream("d:\\zekiri.jpg", FileMode.Create, FileAccess.Write);
+                        try
+                        {
+                            Console.WriteLine("Download complete.");
+                            stream.WriteTo(file);
+                        }
+                        finally
+                        {
+                            file.Close();
+                            stream.Close();
+                        }
+                    }
+                };
+
+                request.Download(stream);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
         }
+
         static void Main(string[] args)
         {
-
-            UserCredential credential = GetUserCredential();
-            
-            // Create Drive API service.
-            var service = new DriveService(new BaseClientService.Initializer()
+            try
             {
-                HttpClientInitializer = credential,
-                ApplicationName = ApplicationName,
-            });
+                UserCredential credential = GetUserCredentials();
 
-            // List files.
-              Getfiles(service);
-            
+                // Create Drive API service.
+                service = new DriveService(new BaseClientService.Initializer()
+                {
+                    HttpClientInitializer = credential,
+                    ApplicationName = APPLICATION_NAME,
+                });
+
+                // List files.
+                Getfiles();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
         }
 
     }
---
+++ VS2019/S3ApplicationConsole/S3ApplicationConsole/Program.cs
@@ -0,0 +1,106 @@
+﻿using Amazon;
+using Amazon.Runtime;
+using Amazon.S3;
+using Amazon.S3.Model;
+using System;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace S3ApplicationConsole
+{
+    class Program
+    {
+        //QA Access
+        private static string accessKeyID = "XXXXXXXXXXXXXXXXXXXXXX";
+        private static string secretKey = "YYYYYYYYYYYYYYYYYYYYYY";
+        private static string bucketName = "ZZZZZZZZZZZZZZZZZZZZZ";
+
+        private static readonly string PostDir = @"D:\";
+        private static IAmazonS3 service;
+        private static bool firstfileisOk = false;
+
+        private static void UserCredential()
+        {
+            var credentials = new BasicAWSCredentials(accessKeyID, secretKey);
+            service = new AmazonS3Client(
+                credentials, 
+                RegionEndpoint.USEast1
+                );
+        }
+
+        private static async Task GetFiles()
+        {
+            try
+            {
+                ListObjectsV2Request request = new ListObjectsV2Request 
+                { 
+                    BucketName = bucketName, 
+                    MaxKeys = 10 
+                };
+
+                ListObjectsV2Response response;
+               
+                do
+                {
+                    response = await service.ListObjectsV2Async(request);
+
+                    foreach (S3Object fileobj in response.S3Objects)
+                    {
+                        string[] filePath = fileobj.Key.Split('/');
+
+                        if (filePath.Count() == 3 && filePath[2].Contains('.'))
+                        {
+                            string exten = filePath[2].Split('.')[1];
+                            Console.WriteLine("key = XXXXXXXXXX.{0} size = {1}", exten, fileobj.Size);
+
+                            if (!firstfileisOk)
+                            {
+                                DownloadFile(fileobj.Key).Wait();
+                            }
+                        }
+                    }
+                    request.ContinuationToken = response.NextContinuationToken;
+
+                } while (response.IsTruncated);
+
+                Console.ReadKey();
+            }
+            catch (AmazonS3Exception amazonS3Exception)
+            {
+                Console.WriteLine("S3 error occurred. Exception: " + amazonS3Exception.ToString());
+                Console.ReadKey();
+            }
+        }
+
+
+        public static async Task DownloadFile(string fileId)
+        {
+            string localFile = Guid.NewGuid().ToString() + ".csv";
+            try
+            {
+                GetObjectRequest request = new GetObjectRequest() { BucketName = bucketName, Key = fileId };
+
+                GetObjectResponse response = await service.GetObjectAsync(request);
+
+                await response.WriteResponseStreamToFileAsync(Path.Combine(PostDir, localFile), true, CancellationToken.None);
+
+                firstfileisOk = true;
+            }
+            catch (AmazonS3Exception amazonS3Exception)
+            {
+                Console.WriteLine(amazonS3Exception.ToString());
+            }
+
+        }
+
+        public static void Main()
+        {
+            UserCredential();
+
+            GetFiles().Wait();
+        }
+
+    }
+}
